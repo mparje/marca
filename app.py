cbe73f2f62439fd5752a55e10ce67e6154571e9b
mparje <126131806+mparje@users.noreply.github.com>
2023-06-17 06:07:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import tweepy
+from datetime import datetime
 from textblob import TextBlob
+import pandas as pd
 
 # Función para analizar el sentimiento del texto
 def analizar_sentimiento(texto):
@@ -10,7 +12,7 @@ def analizar_sentimiento(texto):
     return polaridad, subjetividad
 
 # Función para buscar información sobre una marca en Twitter
-def buscar_informacion_marca(marca):
+def buscar_informacion_marca(marca, fecha):
     # Configuración de las credenciales de Twitter API
     consumer_key = "fr9FM4cAAEoMpvak8fKk5kDOp"
     consumer_secret = "7P025Egxnib5dogMvOLgdo1uHJPJg2lx98Rvg3XYvIjf4Eo59u"
@@ -24,31 +26,33 @@ def buscar_informacion_marca(marca):
     resultados = []
     
     # Realizar la búsqueda en Twitter
-    tweets = api.search(q=marca, count=100, lang="es", tweet_mode="extended")
+    tweets = api.search_tweets(q=marca, count=100, lang="es", tweet_mode="extended")
     
     for tweet in tweets:
-        texto = tweet.full_text
-        polaridad, subjetividad = analizar_sentimiento(texto)
-        resultados.append({"usuario": tweet.user.screen_name, "texto": texto, "polaridad": polaridad, "subjetividad": subjetividad})
+        # Obtener la fecha del tweet y convertirla a formato datetime
+        fecha_tweet = tweet.created_at.date()
+        
+        # Verificar si el tweet se publicó en la fecha especificada
+        if fecha_tweet == fecha:
+            texto = tweet.full_text
+            polaridad, subjetividad = analizar_sentimiento(texto)
+            resultados.append({"Usuario": tweet.user.screen_name, "Texto": texto, "Polaridad": polaridad, "Subjetividad": subjetividad})
 
     return resultados
 
 # Configuración de la aplicación Streamlit
 st.title("Análisis de Sentimientos y Búsqueda de Información de Marcas")
 marca = st.text_input("Ingrese una marca:")
+fecha = st.date_input("Seleccione una fecha:")
 
 if st.button("Analizar"):
     if marca:
-        resultados = buscar_informacion_marca(marca)
+        resultados = buscar_informacion_marca(marca, fecha)
         if resultados:
-            st.write(f"Resultados encontrados para la marca '{marca}':")
-            for resultado in resultados:
-                st.write(f"Usuario: {resultado['usuario']}")
-                st.write(f"Texto: {resultado['texto']}")
-                st.write(f"Polaridad: {resultado['polaridad']}")
-                st.write(f"Subjetividad: {resultado['subjetividad']}")
-                st.write("---")
+            df = pd.DataFrame(resultados)
+            st.write(f"Resultados encontrados para la marca '{marca}' en la fecha {fecha}:")
+            st.dataframe(df)
         else:
-            st.write("No se encontraron resultados para la marca especificada.")
+            st.write("No se encontraron resultados para la marca y fecha especificadas.")
     else:
         st.write("Por favor, ingrese una marca.")
