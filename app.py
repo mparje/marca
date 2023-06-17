9d80ae7b5db8a5d8a8ab59c9ac02b83b248f9ab5
mparje <126131806+mparje@users.noreply.github.com>
2023-06-17 04:57:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,27 +2,27 @@ import streamlit as st
 import tweepy
 from datetime import datetime
 from textblob import TextBlob
+import matplotlib.pyplot as plt
 
 # Función para analizar el sentimiento del texto
 def analizar_sentimiento(texto):
     blob = TextBlob(texto)
     polaridad = blob.sentiment.polarity
-    subjetividad = blob.sentiment.subjectivity
-    return polaridad, subjetividad
+    return polaridad
 
 # Función para buscar información sobre una marca en Twitter
 def buscar_informacion_marca(marca, fecha):
-    # Configuración de las credenciales de Twitter API
-    consumer_key = "fr9FM4cAAEoMpvak8fKk5kDOp"
-    consumer_secret = "7P025Egxnib5dogMvOLgdo1uHJPJg2lx98Rvg3XYvIjf4Eo59u"
-    access_token = "113763054-GQQ7L2Pa9yfhX386G0khrBhecdTMgIgpadwGglDv"
-    access_token_secret = "o5ggZAOJrq1AotVlg2gGJ7MpVpHnh0eg4xYsKX2EsbFvT"
+    # Acceder a las credenciales de Twitter API desde los secrets de Streamlit
+    consumer_key = st.secrets["twitter"]["consumer_key"]
+    consumer_secret = st.secrets["twitter"]["consumer_secret"]
+    access_token = st.secrets["twitter"]["access_token"]
+    access_token_secret = st.secrets["twitter"]["access_token_secret"]
     
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
-    resultados = []
+    resultados = {"Positivo": 0, "Negativo": 0, "Neutral": 0}
     
     # Realizar la búsqueda en Twitter
     tweets = api.search_tweets(q=marca, count=100, lang="es", tweet_mode="extended")
@@ -34,8 +34,14 @@ def buscar_informacion_marca(marca, fecha):
         # Verificar si el tweet se publicó en la fecha especificada
         if fecha_tweet == fecha:
             texto = tweet.full_text
-            polaridad, subjetividad = analizar_sentimiento(texto)
-            resultados.append({"usuario": tweet.user.screen_name, "texto": texto, "polaridad": polaridad, "subjetividad": subjetividad})
+            polaridad = analizar_sentimiento(texto)
+            
+            if polaridad > 0:
+                resultados["Positivo"] += 1
+            elif polaridad < 0:
+                resultados["Negativo"] += 1
+            else:
+                resultados["Neutral"] += 1
 
     return resultados
 
@@ -49,12 +55,17 @@ if st.button("Analizar"):
         resultados = buscar_informacion_marca(marca, fecha)
         if resultados:
             st.write(f"Resultados encontrados para la marca '{marca}' en la fecha {fecha}:")
-            for resultado in resultados:
-                st.write(f"Usuario: {resultado['usuario']}")
-                st.write(f"Texto: {resultado['texto']}")
-                st.write(f"Polaridad: {resultado['polaridad']}")
-                st.write(f"Subjetividad: {resultado['subjetividad']}")
-                st.write("---")
+            st.write(resultados)
+            
+            # Generar la gráfica de pie
+            labels = resultados.keys()
+            values = resultados.values()
+            
+            fig, ax = plt.subplots()
+            ax.pie(values, labels=labels, autopct='%1.1f%%')
+            ax.set_title("Análisis de Sentimientos")
+            
+            st.pyplot(fig)
         else:
             st.write("No se encontraron resultados para la marca y fecha especificadas.")
     else:
