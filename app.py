833ae2a1658fbbd9799bd474b5d8a1c5e4cf2443
mparje <126131806+mparje@users.noreply.github.com>
2023-06-17 06:03:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import tweepy
-from datetime import datetime
 from textblob import TextBlob
+import matplotlib.pyplot as plt
 
 # Función para analizar el sentimiento del texto
 def analizar_sentimiento(texto):
@@ -11,7 +11,7 @@ def analizar_sentimiento(texto):
     return polaridad, subjetividad
 
 # Función para buscar información sobre una marca en Twitter
-def buscar_informacion_marca(marca, fecha):
+def buscar_informacion_marca(marca):
     # Configuración de las credenciales de Twitter API
     consumer_key = "fr9FM4cAAEoMpvak8fKk5kDOp"
     consumer_secret = "7P025Egxnib5dogMvOLgdo1uHJPJg2lx98Rvg3XYvIjf4Eo59u"
@@ -25,37 +25,44 @@ def buscar_informacion_marca(marca, fecha):
     resultados = []
     
     # Realizar la búsqueda en Twitter
-    tweets = api.search_tweets(q=marca, count=100, lang="es", tweet_mode="extended")
+    tweets = api.search(q=marca, count=100, lang="es", tweet_mode="extended")
     
     for tweet in tweets:
-        # Obtener la fecha del tweet y convertirla a formato datetime
-        fecha_tweet = tweet.created_at.date()
-        
-        # Verificar si el tweet se publicó en la fecha especificada
-        if fecha_tweet == fecha:
-            texto = tweet.full_text
-            polaridad, subjetividad = analizar_sentimiento(texto)
-            resultados.append({"usuario": tweet.user.screen_name, "texto": texto, "polaridad": polaridad, "subjetividad": subjetividad})
+        texto = tweet.full_text
+        polaridad, subjetividad = analizar_sentimiento(texto)
+        resultados.append({"usuario": tweet.user.screen_name, "texto": texto, "polaridad": polaridad, "subjetividad": subjetividad})
 
     return resultados
 
 # Configuración de la aplicación Streamlit
 st.title("Análisis de Sentimientos y Búsqueda de Información de Marcas")
 marca = st.text_input("Ingrese una marca:")
-fecha = st.date_input("Seleccione una fecha:")
 
 if st.button("Analizar"):
     if marca:
-        resultados = buscar_informacion_marca(marca, fecha)
+        resultados = buscar_informacion_marca(marca)
         if resultados:
-            st.write(f"Resultados encontrados para la marca '{marca}' en la fecha {fecha}:")
+            st.write(f"Resultados encontrados para la marca '{marca}':")
             for resultado in resultados:
                 st.write(f"Usuario: {resultado['usuario']}")
                 st.write(f"Texto: {resultado['texto']}")
                 st.write(f"Polaridad: {resultado['polaridad']}")
                 st.write(f"Subjetividad: {resultado['subjetividad']}")
                 st.write("---")
+
+            # Generar la gráfica de pastel
+            polaridades = [resultado['polaridad'] for resultado in resultados]
+            labels = ['Positivo', 'Neutral', 'Negativo']
+            values = [len([p for p in polaridades if p > 0]),
+                      len([p for p in polaridades if p == 0]),
+                      len([p for p in polaridades if p < 0])]
+            
+            fig, ax = plt.subplots()
+            ax.pie(values, labels=labels, autopct='%1.1f%%')
+            ax.set_title("Análisis de Sentimientos")
+            
+            st.pyplot(fig)
         else:
-            st.write("No se encontraron resultados para la marca y fecha especificadas.")
+            st.write("No se encontraron resultados para la marca especificada.")
     else:
         st.write("Por favor, ingrese una marca.")
